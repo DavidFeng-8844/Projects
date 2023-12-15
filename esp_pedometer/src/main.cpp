f7e78a52c6f9ee9c4c26c31bb20442d6ee7a9e18
davidfeng <davidfeng.8844@gmail.com>
2023-12-15 17:05:58 +0800

    Update overtime count

--- esp_pedometer/src/main.cpp
+++ esp_pedometer/src/main.cpp
@@ -112,7 +112,7 @@ void loop() {
     if (step_valid > STEP_OK - 1) { //if the current step count is greater than 8
       if (GetTime() - lastTime > 10000) {   //if the current time minus the last time is greater than 10 seconds
         walkSta = false;                  //set the walk state to false
-        step_valid = 0;
+        step_valid = 1;
         display.clearDisplay();
         display.setTextSize(1.2); // Normal 1:1 pixel scale
         display.setTextColor(SSD1306_WHITE); // Draw white text
